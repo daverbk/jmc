8367098f22049ce987c778cd332eb9f28e8dc28d
Dave Rabko <davidryabko13@gmail.com>
2023-10-28 19:17:01 +0300

    Finish oop part 1

--- oop1/src/main/java/StringFormatting.java
+++ oop1/src/main/java/StringFormatting.java
@@ -13,8 +13,36 @@ public class StringFormatting {
         System.out.printf("index of l = %d %n", helloWorld.indexOf('l'));
         System.out.printf("index of l = %d %n", helloWorld.lastIndexOf('l'));
 
-        System.out.printf("index of l = %d %n", helloWorld.indexOf('l', 3));
-        System.out.printf("index of l = %d %n", helloWorld.lastIndexOf('l', 8));
+        System.out.printf("index of l = %d %n", helloWorld.indexOf('l',
+                3));
+        System.out.printf("index of l = %d %n", helloWorld.lastIndexOf('l',
+                8));
+
+        String helloWorldLower = helloWorld.toLowerCase();
+
+        if (helloWorld.equals(helloWorldLower)) {
+            System.out.println("Value match exactly");
+        }
+
+        if (helloWorld.equalsIgnoreCase(helloWorldLower)) {
+            System.out.println("Value match ignoring exactly");
+        }
+
+        if (helloWorld.startsWith("Hello")) {
+            System.out.println("String starts with Hello");
+        }
+
+        if (helloWorld.startsWith("World")) {
+            System.out.println("String ends with World");
+        }
+
+        if (helloWorld.contains("World")) {
+            System.out.println("String contains with World");
+        }
+
+        if (helloWorld.contentEquals("Hello World")) {
+            System.out.println("Value match exactly");
+        }
     }
 
     public static void printInformation(String string) {
---
+++ oop1/src/main/java/poolarea/Cuboid.java
@@ -0,0 +1,18 @@
+package poolarea;
+
+public class Cuboid extends Rectangle {
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = height > 0 ? height : 0;
+    }
+
+    public double getVolume() {
+        return height * getArea();
+    }
+
+    public double getHeight() {
+        return height;
+    }
+}
