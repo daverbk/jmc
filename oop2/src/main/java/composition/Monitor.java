f6ced50f18c04311d973d7edd1c44a032899d3d0
Dave Rabko <davidryabko13@gmail.com>
2023-10-30 23:36:06 +0100

    Add oop part 2

---
+++ oop2/src/main/java/composition/Monitor.java
@@ -0,0 +1,21 @@
+package composition;
+
+public class Monitor extends Product {
+
+    private int size;
+    private String resolution;
+
+    public Monitor(String model, String manufacturer) {
+        super(model, manufacturer);
+    }
+
+    public Monitor(String model, String manufacturer, int size, String resolution) {
+        super(model, manufacturer);
+        this.size = size;
+        this.resolution = resolution;
+    }
+
+    public void drawPixelAt(int x, int y, String color) {
+        System.out.printf("Drawing pixel at %d, %d in color %s ", x, y, color);
+    }
+}
