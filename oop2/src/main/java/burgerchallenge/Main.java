df3d25d183833facde19bede3bec09c80b046616
David Rabko <david.rabko@icloud.com>
2023-11-06 21:59:45 +0100

    Add deluxe burger

--- oop2/src/main/java/burgerchallenge/Main.java
+++ oop2/src/main/java/burgerchallenge/Main.java
@@ -1,6 +1,7 @@
 package burgerchallenge;
 
 import burgerchallenge.burgerstuff.Burger;
+import burgerchallenge.burgerstuff.DeluxeBurger;
 import burgerchallenge.burgerstuff.Topping;
 
 public class Main {
@@ -14,12 +15,18 @@ public class Main {
         Burger burger = new Burger("Cheese Burger", 9.99);
         Drink drink = new Drink("Diet Coke", "Large");
         SideDish sideDish = new SideDish("Salad", 3.99);
-        Meal secondMeal = new Meal(burger, sideDish, drink);
+        Meal meal2 = new Meal(burger, sideDish, drink);
 
         // Add a topping
         Topping chickenTopping = new Topping("Chicken Topping", 2.99);
-        secondMeal.addBurgerTopping(chickenTopping);
-        printOrderData(secondMeal);
+        meal2.addBurgerTopping(chickenTopping);
+        printOrderData(meal2);
+
+        // Create a deluxe burger
+        Burger deluxeBurger = new DeluxeBurger("Big Burger", 19.99);
+        Meal meal3 = new Meal(deluxeBurger, sideDish, drink);
+        meal3.addBurgerTopping(chickenTopping);
+        printOrderData(meal3);
     }
 
     public static void printOrderData(Meal order) {
