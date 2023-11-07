30ad9e97b313a2e3db266474689dd5883b3578a1
Dave Rabko <davidryabko13@gmail.com>
2023-11-07 22:38:58 +0100

    Amend burger shop

--- oop2/src/main/java/burgerchallenge/Main.java
+++ oop2/src/main/java/burgerchallenge/Main.java
@@ -1,36 +1,20 @@
 package burgerchallenge;
 
-import burgerchallenge.burgerstuff.Burger;
-import burgerchallenge.burgerstuff.DeluxeBurger;
-import burgerchallenge.burgerstuff.Topping;
-
 public class Main {
+
     public static void main(String[] args) {
+        /*Item coke = new Item("drink", "coke", 1.50);
+        coke.printItem();
+        coke.setSize("LARGE");
+        coke.printItem();
 
-        // Default meal
-        Meal defaultMeal = new Meal();
-        printOrderData(defaultMeal);
+        Item avocado = new Item("Topping", "avocado", 1.50);
+        avocado.printItem();*/
 
-        // Create a meal with a burger and a drink and side item with up to 3 toppings
-        Burger burger = new Burger("Cheese Burger", 9.99);
-        Drink drink = new Drink("Diet Coke", "Large");
-        SideDish sideDish = new SideDish("Salad", 3.99);
-        Meal meal2 = new Meal(burger, sideDish, drink);
+        Burger burger = new Burger("regular", 4.0);
+        burger.addToppings("BACON", "CHEESE", "MAYO");
+        burger.printItem();
 
-        // Add a topping
-        Topping chickenTopping = new Topping("Chicken Topping", 2.99);
-        meal2.addBurgerTopping(chickenTopping);
-        printOrderData(meal2);
 
-        // Create a deluxe burger
-        Burger deluxeBurger = new DeluxeBurger("Big Burger", 19.99);
-        Meal meal3 = new Meal(deluxeBurger, sideDish, drink);
-        meal3.addBurgerTopping(chickenTopping);
-        printOrderData(meal3);
-    }
-
-    public static void printOrderData(Meal order) {
-        order.list();
-        order.printTotal();
     }
 }
