fded01470e0183521c2729d0267765b764558930
David Rabko <david.rabko@icloud.com>
2023-11-07 22:38:58 +0100

    Amend burger shop

--- oop2/src/main/java/burgerchallenge/SideDish.java
+++
@@ -1,7 +0,0 @@
-package burgerchallenge;
-
-public class SideDish extends Product {
-    public SideDish(String type, double price) {
-        super(type, price);
-    }
-}
--- oop2/src/main/java/burgerchallenge/burgerstuff/Topping.java
+++
@@ -1,9 +0,0 @@
-package burgerchallenge.burgerstuff;
-
-import burgerchallenge.Product;
-
-public class Topping extends Product {
-    public Topping(String type, double price) {
-        super(type, price);
-    }
-}
