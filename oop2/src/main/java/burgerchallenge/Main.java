d8c3ed0ee2bbea8c82272b48b9ccb041b9f8f256
David Rabko <david.rabko@icloud.com>
2023-11-08 12:46:23 +0100

    Finish oop2

--- oop2/src/main/java/burgerchallenge/Main.java
+++ oop2/src/main/java/burgerchallenge/Main.java
@@ -3,27 +3,31 @@ package burgerchallenge;
 public class Main {
 
     public static void main(String[] args) {
-        /*Item coke = new Item("drink", "coke", 1.50);
+        Item coke = new Item("drink", "coke", 1.50);
         coke.printItem();
         coke.setSize("LARGE");
         coke.printItem();
 
         Item avocado = new Item("Topping", "avocado", 1.50);
-        avocado.printItem();*/
+        avocado.printItem();
 
-        /*Burger burger = new Burger("regular", 4.0);
+        Burger burger = new Burger("regular", 4.0);
         burger.addToppings("BACON", "CHEESE", "MAYO");
-        burger.printItem();*/
+        burger.printItem();
 
-        /*MealOrder regularMeal = new MealOrder();
+        MealOrder regularMeal = new MealOrder();
         regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
         regularMeal.setDrinkSize("LARGE");
         regularMeal.printItemizedList();
-        */
 
         MealOrder secondMeal = new MealOrder("turkey", "7-up", "chili");
         secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
         secondMeal.setDrinkSize("SMALL");
         secondMeal.printItemizedList();
+
+        MealOrder deluxeMeal = new MealOrder("deluxe", "7-up", "chili");
+        deluxeMeal.addBurgerToppings("AVOCADO", "BACON", "LETTUCE", "CHEESE", "MAYO");
+        deluxeMeal.setDrinkSize("SMALL");
+        deluxeMeal.printItemizedList();
     }
 }
