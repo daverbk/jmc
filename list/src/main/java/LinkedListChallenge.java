1ba47397df25b64ca7edf940904e302f99ba5f49
Dave Rabko <davidryabko13@gmail.com>
2023-11-24 22:18:26 +0100

    Start playlist coding task

---
+++ list/src/main/java/LinkedListChallenge.java
@@ -0,0 +1,121 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+record Place(String name, int distance) {
+
+    @Override
+    public String toString() {
+        return String.format("%s (%d)", name, distance);
+    }
+}
+
+public class LinkedListChallenge {
+
+    public static void main(String[] args) {
+
+        LinkedList<Place> placesToVisit = new LinkedList<>();
+        addPlace(placesToVisit, new Place("Adelaide", 1374));
+        addPlace(placesToVisit, new Place("Brisbane", 917));
+        addPlace(placesToVisit, new Place("Perth", 3923));
+        addPlace(placesToVisit, new Place("Alice Springs", 2771));
+        addPlace(placesToVisit, new Place("Darwin", 3972));
+        addPlace(placesToVisit, new Place("Melbourne", 877));
+
+        placesToVisit.addFirst(new Place("Sydney", 0));
+        System.out.println(placesToVisit);
+
+        var iterator = placesToVisit.listIterator();
+        Scanner scanner = new Scanner(System.in);
+        boolean quitLoop = false;
+        boolean forward = true;
+
+        printMenu();
+
+        while (!quitLoop) {
+            if (!iterator.hasPrevious()) {
+                System.out.println("Originating: " + iterator.next());
+                forward = true;
+            }
+
+            if (!iterator.hasNext()) {
+                System.out.println("Final: " + iterator.previous());
+                forward = false;
+            }
+
+            System.out.println("Enter Value: ");
+            String menuItem = scanner.nextLine().toUpperCase().substring(0, 1);
+
+            switch (menuItem) {
+                case "F":
+                    System.out.println("User wants to go forward");
+                    if (!forward) {
+                        forward = true;
+                        if (iterator.hasNext()) {
+                            iterator.next();
+                        }
+                    }
+
+                    if (iterator.hasNext()) {
+                        System.out.println(iterator.next());
+                    }
+
+                    break;
+                case "B":
+                    System.out.println("User wants to go backwards");
+                    if (forward) {
+                        forward = false;
+                        if (iterator.hasPrevious()) {
+                            iterator.previous();
+                        }
+                    }
+
+                    if (iterator.hasPrevious()) {
+                        System.out.println(iterator.previous());
+                    }
+
+                    break;
+                case "M":
+                    printMenu();
+                    break;
+                case "L":
+                    System.out.println(placesToVisit);
+                    break;
+                default:
+                    quitLoop = true;
+                    break;
+            }
+        }
+    }
+
+    private static void addPlace(LinkedList<Place> list, Place place) {
+
+        for (Place p : list) {
+            if (p.name().equalsIgnoreCase(place.name())) {
+                System.out.println("Found duplicate: " + place);
+                return;
+            }
+        }
+
+        int matchedIndex = 0;
+        for (Place p : list) {
+            if (place.distance() < p.distance()) {
+                list.add(matchedIndex, place);
+                return;
+            }
+
+            matchedIndex++;
+        }
+
+        list.add(place);
+    }
+
+    private static void printMenu() {
+        System.out.print("""
+                Available actions (select word or letter):
+                (F)orward
+                (B)ackwards
+                (L)ist Places
+                (M)enu
+                (Q)uit""");
+    }
+}
