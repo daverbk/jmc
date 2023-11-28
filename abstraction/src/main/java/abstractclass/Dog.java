e447b7deeda5f7a5fdb697aa6457fef7eacfc447
Dave Rabko <davidryabko13@gmail.com>
2023-11-28 22:17:40 +0100

    Add abstraction

---
+++ abstraction/src/main/java/abstractclass/Dog.java
@@ -0,0 +1,27 @@
+package abstractclass;
+
+public class Dog extends Animal {
+
+    public Dog(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+
+        if (speed.equals("slow")) {
+            System.out.println(type + " walking");
+        } else {
+            System.out.println(type + " running");
+        }
+    }
+
+    @Override
+    public void makeNoise() {
+        if (type == "Wolf") {
+            System.out.println("Howling! ");
+        } else {
+            System.out.println("Woof! ");
+        }
+    }
+}
