7d67633cee97d1e4922bb7342d7659c703ee16fa
David Rabko <david.rabko@icloud.com>
2023-11-28 22:17:40 +0100

    Add abstraction

---
+++ abstraction/src/main/java/abstractclass/Fish.java
@@ -0,0 +1,27 @@
+package abstractclass;
+
+public class Fish extends Animal {
+
+    public Fish(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+
+        if (speed.equals("slow")) {
+            System.out.println(type + " lazily swimming");
+        } else {
+            System.out.println(type + " darting frantically");
+        }
+    }
+
+    @Override
+    public void makeNoise() {
+        if (type == "Goldfish") {
+            System.out.println("swish ");
+        } else {
+            System.out.println("splash ");
+        }
+    }
+}
