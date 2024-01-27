6694347486699252a2804b1789a9b9bc284bcf2f
David Rabko <david.rabko@icloud.com>
2024-01-27 22:56:19 +0100

    Add nested classes

---
+++ nestedclasses/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import domain.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>(List.of(
+                new Employee(10001, "Ralph", 2015),
+                new Employee(10005, "Carole", 2021),
+                new Employee(10022, "Jane", 2013),
+                new Employee(13151, "Laura", 2020),
+                new Employee(10050, "Jim", 2018)));
+
+        employees.sort(new Employee.EmployeeComparator<>("yearStarted").reversed());
+
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+}
