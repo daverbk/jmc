df5b0412e6a17f6da1e7cf6c8e36299cb493640e
David Rabko <davidryabko13@gmail.com>
2024-03-03 15:45:29 +0100

    Start hash codes studies

---
+++ 13-collections/src/main/java/hashing/Main.java
@@ -0,0 +1,35 @@
+package hashing;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String aText = "Hello";
+        String bText = "Hello";
+        String cText = String.join("l","He", "lo");
+        String dText = "He".concat("llo");
+        String eText = "hello";
+
+        List<String> hellos = Arrays.asList(aText, bText, cText, dText, eText);
+        hellos.forEach(s -> System.out.println(s + ": " + s.hashCode()));
+
+        Set<String> mySet = new HashSet<>(hellos);
+
+        System.out.println("mySet = " + mySet);
+        System.out.println("# of elements = " + mySet.size());
+
+        for (String setValue : mySet) {
+            System.out.print(setValue + ": ");
+            for (int i = 0; i < hellos.size(); i++) {
+                if (setValue == hellos.get(i)) {
+                    System.out.print(i + ", ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
