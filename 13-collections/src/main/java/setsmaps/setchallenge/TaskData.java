9f8449e81e797775d69f0f7b996b63fc30fb7052
David Rabko <david.rabko@icloud.com>
2024-03-15 11:19:08 +0100

    Add hashset challenge

---
+++ 13-collections/src/main/java/setsmaps/setchallenge/TaskData.java
@@ -0,0 +1,72 @@
+package setsmaps.setchallenge;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TaskData {
+    private static final String tasks = """
+            Infrastructure, Logging, High
+            Infrastructure, DB Access, Medium
+            Infrastructure, Security, High
+            Infrastructure, Password Policy, Medium
+            Data Design, Task Table, Medium
+            Data Design, Employee Table, Medium
+            Data Design, Cross Reference Tables, High
+            Data Design, Encryption Policy, High
+            Data Access, Write Views, Low
+            Data Access, Set Up Users, Low
+            Data Access, Set Up Access Policy, Low
+            """;
+
+    private static final String annsTasks = """
+            Infrastructure, Security, High, In Progress
+            Infrastructure, Password Policy,Medium, In Progress
+            Research, Cloud solutions, Medium, In Progress
+            Data Design, Encryption Policy, High
+            Data Design, Project Table, Medium
+            Data Access, Write Views,Low, In Progress
+            """;
+
+    private static final String bobsTasks = """
+            Infrastructure, Security, High, In Progress
+            Infrastructure, Password Policy, Medium
+            Data Design,Encryption Policy,High
+            Data Access,Write Views, Low, In Progress
+            """;
+
+    private static final String carolsTasks = """
+            Infrastructure, Logging, High, In Progress
+            Infrastructure, DB Access, Medium
+            Infrastructure, Password Policy, Medium
+            Data Design, Task Table, High
+            Data Access, Write Views, Low
+            """;
+
+    public static Set<Task> getTasks(String owner) {
+
+        Set<Task> taskList = new HashSet<>();
+        String user = ("ann, bob, carol".contains(owner.toLowerCase())) ? owner : null;
+
+        String selectedList = switch (owner.toLowerCase()) {
+            case "ann" -> annsTasks;
+            case "bob" -> bobsTasks;
+            case "carol" -> carolsTasks;
+            default -> tasks;
+        };
+
+        for (String taskData : selectedList.split("\n")) {
+            String[] data = taskData.split(",");
+            Arrays.asList(data).replaceAll(String::trim);
+
+            Status status = data.length <= 3 ? Status.IN_QUEUE :
+                    Status.valueOf(data[3].toUpperCase()
+                            .replace(' ', '_'));
+
+            Priority priority = Priority.valueOf(data[2].toUpperCase());
+            taskList.add(new Task(data[0], data[1], user, priority, status));
+        }
+
+        return taskList;
+    }
+}
