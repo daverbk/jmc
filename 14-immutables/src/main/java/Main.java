44c38380aa322b96467af1597f0bf51305bf81b9
David Rabko <davidryabko13@gmail.com>
2024-06-03 23:57:07 +0200

    Continue immutables

--- 14-immutables/src/main/java/Main.java
+++ 14-immutables/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import consumer.specific.ChildClass;
+import external.util.Logger;
 import finalmodifier.BaseClass;
 
 public class Main {
@@ -20,5 +21,29 @@ public class Main {
         childClass.recommendedStatic();
         System.out.println("=====================================");
         childReferredToAsBase.recommendedStatic();
+
+        System.out.println("=====================================");
+        BaseClass.recommendedStatic();
+        ChildClass.recommendedStatic();
+
+        String xArgument = "This is all I've got to say about section ";
+        StringBuilder zArgument = new StringBuilder("only saying this after section ");
+        doXYZ(xArgument, 16, zArgument);
+        System.out.println("after method xArgument = " + xArgument);
+        System.out.println("after method zArgument = " + zArgument);
+
+        StringBuilder tracker = new StringBuilder("Step 1 is abc");
+        Logger.logToConsole(tracker.toString());
+        tracker.append("Step 2 is xyz");
+        Logger.logToConsole(tracker.toString());
+        System.out.println("after logging tracker = " + tracker);
+    }
+
+    private static void doXYZ(String x, int y, final StringBuilder z) {
+
+        final String c = x + y;
+        System.out.println("c = " + c);
+        x = c;
+//        z = new StringBuilder("I'm saying this after section " + y);
     }
 }
