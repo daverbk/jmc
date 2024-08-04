79bdc1e4cd572694da83e78e66d482eee736dd7e
David Rabko <david.rabko@icloud.com>
2024-08-05 00:10:27 +0200

    Finish streams sources challenge

--- 15-streams/src/main/java/sourcechallenge/Main.java
+++ 15-streams/src/main/java/sourcechallenge/Main.java
@@ -1,9 +1,13 @@
 package sourcechallenge;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.stream.Stream;
 
 public class Main {
+
+    static int counter = 0;
+
     public static void main(String[] args) {
         int seed = 1;
         var streamB = Stream.iterate(seed, i -> i <= 15, i -> i + 1).map(i -> "B" + i);
@@ -16,6 +20,28 @@ public class Main {
         String[] oLabels = new String[15];
         Arrays.setAll(oLabels, i -> "O" + (nSeed + i));
         var streamN = Arrays.stream(oLabels);
-        streamN.forEach(System.out::println);
+
+        seed += 15;
+        var streamG = Stream.of("G46", "G47", "G48", "G49", "G50", "G51", "G52", "G53", "G54", "G55", "G56", "G57", "G58", "G59", "G60");
+
+        seed += 15;
+        var rSeed = seed;
+        var streamO = Stream.generate(Main::getCounter).limit(15).map(i -> "O" + (rSeed + i));
+
+        var streamBI = Stream.concat(streamB, streamI);
+        var streamNG = Stream.concat(streamN, streamG);
+        var streamBING = Stream.concat(streamBI, streamNG);
+        Stream.concat(streamBING, streamO).forEach(System.out::println);
+
+        System.out.println("-".repeat(20));
+        Stream.generate(() -> new Random().nextInt(rSeed, rSeed + 15))
+                .distinct()
+                .limit(15)
+                .map(i -> "O" + i)
+                .forEach(System.out::println);
+    }
+
+    private static int getCounter() {
+        return counter++;
     }
 }
