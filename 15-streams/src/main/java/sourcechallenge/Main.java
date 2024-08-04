fb5f36f843071a2e27f29d229a15be910bdd027e
David Rabko <david.rabko@icloud.com>
2024-08-04 23:56:54 +0200

    Add streams

---
+++ 15-streams/src/main/java/sourcechallenge/Main.java
@@ -0,0 +1,21 @@
+package sourcechallenge;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        int seed = 1;
+        var streamB = Stream.iterate(seed, i -> i <= 15, i -> i + 1).map(i -> "B" + i);
+
+        seed += 15;
+        var streamI = Stream.iterate(seed, i -> i + 1).limit(15).map(i -> "I" + i);
+
+        seed += 15;
+        int nSeed = seed;
+        String[] oLabels = new String[15];
+        Arrays.setAll(oLabels, i -> "O" + (nSeed + i));
+        var streamN = Arrays.stream(oLabels);
+        streamN.forEach(System.out::println);
+    }
+}
