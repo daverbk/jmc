66b10afdec09623a788084bf8fa644053092c748
David <davidryabko13@gmail.com>
2025-03-31 23:03:25 +0300

    Start second threads task

--- 19-concurrency/src/main/java/threads/lesson4/BankAccount.java
+++ 19-concurrency/src/main/java/threads/lesson4/BankAccount.java
@@ -3,9 +3,25 @@ package threads.lesson4;
 public class BankAccount {
 
     private double balance;
+    private String name;
 
-    public BankAccount(double balance) {
+    private final Object lockName = new Object();
+    private final Object lockBalance = new Object();
+
+    public BankAccount(String name, double balance) {
         this.balance = balance;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        synchronized (lockName) {
+            this.name = name;
+            System.out.println("Updated name = " + this.name);
+        }
     }
 
     public double getBalance() {
@@ -14,18 +30,31 @@ public class BankAccount {
 
     public void deposit(double amount) {
         try {
-            Thread.sleep(100);
+            System.out.println("Deposit - Talking to the teller ...");
+            Thread.sleep(7000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        double origBalance = balance;
-        balance += amount;
-        System.out.printf("STARTING BALANCE: %.0f, DEPOSIT (%.0f) : NEW BALANCE = %.0f%n",
-            origBalance, amount, balance);
+        synchronized (lockBalance) {
+            double origBalance = balance;
+            balance += amount;
+            System.out.printf("STARTING BALANCE: %.0f, DEPOSIT (%.0f) : NEW BALANCE = %.0f%n",
+                origBalance, amount, balance);
+            addPromoDollars(amount);
+        }
     }
 
-    public void withdraw(double amount) {
+    private void addPromoDollars(double amount) {
+        if (amount >= 5000) {
+            synchronized (lockBalance) {
+                System.out.println("Congratulations... promotion...");
+                balance += 25;
+            }
+        }
+    }
+
+    public synchronized void withdraw(double amount) {
         try {
             Thread.sleep(100);
         } catch (InterruptedException e) {
